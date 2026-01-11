4a9fe48e3b912a2a8acb53878dbf9bb69cd87f87
Anton <muhammadanton340@gmail.com>
2026-01-11 17:46:39 +0700

    first commit

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,42 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+          Data Product
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <table class="w-full">
+                        <thead class="bg-gray-100">
+                            <tr>
+                                <td class="p-3">NO</td>
+                            <td class="p-3">NAMA PRODUK</td>
+                            <td class="p-3">DESKRIPSI</td>
+                            <td class="p-3">HARGA</td>
+                            <td class="p-3">STOK</td>
+                            <td class="p-3">AKSI</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($products as $p)
+                            <tr class="border-b">
+                            <td class="p-3">{{ $loop->iteration }}</td>
+                            <td class="p-3">{{ $p->name }}</td>
+                            <td class="p-3">{{ $p->description }}</td>
+                            <td class="p-3">{{ $p->price }}</td>
+                            <td class="p-3">{{ $p->stock }}</td>
+                            <td class="p-3">
+
+                            </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
