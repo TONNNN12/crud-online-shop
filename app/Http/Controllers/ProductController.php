12616d4aefc049f4c975876485ff7c7c3f767ce5
Anton <muhammadanton340@gmail.com>
2026-01-11 17:54:24 +0700

    memperbaiki controller product=Undefined variable

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $product = Product::all();
+        $products = Product::all();
         return view('product.index', compact('products'));
     }
 
